aeb2da2559fd5b964a42d2d259f04e2710f5b3f7
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2024-03-06 12:14:30 +0500

    Update LAB_2.cpp

--- LAB_2.cpp
+++ LAB_2.cpp
@@ -4,6 +4,15 @@
 
 using namespace std;
 
+struct Token {
+    string name;
+    string type;
+};
+
+const int MAX_TOKENS = 500;
+int currentTokenPosition = 0;
+Token tokenArray[MAX_TOKENS];
+
 #define BUFFER_SIZE 4096
 
 char buffer1[BUFFER_SIZE];
@@ -11,10 +20,12 @@ char buffer2[BUFFER_SIZE];
 
 char* currentBuffer = nullptr;
 
+int state = 0;
+
 FILE* file = nullptr;
 
 void openFile() {
-    char filename[] = "LAB_1.cpp";
+    char filename[] = "dummy.cpp";
     // cout << "Enter the file name: ";
     // cin >> filename;
 
@@ -30,23 +41,90 @@ void openFile() {
 
 void readFile() {
     if (file != nullptr) {
-        size_t bytesRead = fread(buffer1, 1, BUFFER_SIZE, file);
-
-        if (bytesRead < BUFFER_SIZE) {
-            if (feof(file)) {
-                cout << "End of file reached." << endl;
-            } else {
-                cout << "Error reading from file." << endl;
-            }
+        if (currentBuffer == nullptr || currentBuffer == buffer2) {
+            size_t bytesRead = fread(buffer1, 1, BUFFER_SIZE, file);
+            currentBuffer = buffer1;
+        } else {
+            size_t bytesRead = fread(buffer2, 1, BUFFER_SIZE, file);
+            currentBuffer = buffer2;
         }
     }
-    currentBuffer = buffer1;
-    return;
+}
+
+void tokenGen(string tokenName, string tokenType) {
+    Token newToken;
+    newToken.name = tokenName;
+    newToken.type = tokenType;
+
+    tokenArray[currentTokenPosition] = newToken;
+    currentTokenPosition++;
+}
+
+bool isLetter(char ch) {
+    return ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'));
+}
+
+bool isDigit(char ch) {
+    return (ch >= '0' && ch <= '9');
+}
+
+void identifierFinder() {
+    int bufferIndex = 0;
+    int lexemeIndex = 0;
+
+    char ch = currentBuffer[bufferIndex];
+    char lexeme[50];
+
+    for (bufferIndex = 0; bufferIndex < BUFFER_SIZE; bufferIndex++) {
+        switch (state) {
+            case 0:
+                if (ch == '_' || isLetter(ch)) {
+                    state = 1;
+                    lexeme[lexemeIndex] = ch;
+                    lexemeIndex++;
+                }
+                break;
+
+            case 1:
+                if (ch == '_' || isLetter(ch) || isDigit(ch)) {
+                    state = 1;
+                    lexeme[lexemeIndex] = ch;
+                    lexemeIndex++;
+                } else {
+                    state = 2;
+                }
+                break;
+
+            case 2:
+                lexeme[lexemeIndex] = '\0';
+                string lexemeString = lexeme;
+                state = 0;
+                bufferIndex--;
+                tokenGen(lexemeString, "id");
+                lexemeIndex = 0; // Reset lexemeIndex for the next identifier
+                break;
+        }
+        ch = currentBuffer[bufferIndex];
+        if (bufferIndex == BUFFER_SIZE - 1) {
+            bufferIndex = 0;
+            readFile();
+        }
+    }
+}
+
+void printTokens() {
+    for (int i = 0; i < currentTokenPosition; ++i) {
+        cout << "Token " << i+1 << ":" << endl;
+        cout << "  Name: " << tokenArray[i].name << endl;
+        cout << "  Type: " << tokenArray[i].type << endl;
+    }
 }
 
 int main() {
     openFile();
     readFile();
+    identifierFinder();
+    printTokens();
     
     cout << currentBuffer << endl;
 
